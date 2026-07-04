55cac98c9a8b8a2e8cef601cae4c65e044886596
Milwad <milwad.dev@gmail.com>
2026-07-04 22:46:06 +0330

    Create DiscountResult.php

---
+++ src/Support/DiscountResult.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Binafy\LaravelDiscount\Support;
+
+use Binafy\LaravelDiscount\Models\Discount;
+use Illuminate\Support\Collection;
+
+class DiscountResult
+{
+    public function __construct(
+        public Collection     $discounts,
+        public float $originalAmount,
+        public float $discountAmount,
+    ) {
+    }
+
+    /**
+     * The amount left to pay after the discount, never below zero.
+     */
+    public function payableAmount(): float
+    {
+        return round(max($this->originalAmount - $this->discountAmount, 0), 2);
+    }
+}
